52e8198bfad7af37214e55601fcb5df7f6c456af
holl3 <matthias.holsten@gmail.com>
2015-08-19 16:50:20 +0200

    building docker container and run integration tests

---
+++ app/backend/src/integration-test/java/com/github/yatol/backend/demo/HelloWorldIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.github.yatol.backend.demo;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+public class HelloWorldIntegrationTest {
+
+	public static final String BACKEND_URL = "BACKEND_URL";
+	public static final String BACKEND_URL_DEFAULT = "http://localhost:8080/backend/";
+
+	@Test
+	public void helloWorldJsonTest() throws Exception {
+		// Given
+		String backendUrl = System.getProperty(BACKEND_URL, BACKEND_URL_DEFAULT);
+
+		// When
+		WebResource resource = Client.create().resource(backendUrl + "json");
+		String result = resource.get(String.class);
+
+		// Then
+		assertThat(result).isEqualTo("{\"result\":\"Hello World\"}");
+	}
+
+}
