c047335698db22b786d1be3b35811ab08d68a5d2
Franz Becker <franz.becker@itemis.de>
2015-09-02 09:23:00 +0200

    trying to install vagrant into docker

--- infra/ci/start-jenkins.sh
+++ infra/ci/start-jenkins.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker create --name jenkins -p 8080:8080 -v "/yatol":"/yatol" --add-host dockerhost:`/sbin/ip route | awk '/docker/ {print $9}'` --privileged -p 2375 -e PORT=2375 -t yatol/jenkins /usr/local/bin/jenkins.sh
+docker create --name jenkins -p 8080:8080 -v "/yatol":"/yatol" -v "/home/vagrant/.vagrant.d":"/root/.vagrant.d" --add-host dockerhost:`/sbin/ip route | awk '/docker/ {print $9}'` -t yatol/jenkins /usr/local/bin/jenkins.sh
