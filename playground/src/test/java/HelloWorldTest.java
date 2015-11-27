891d26d803523fb8e4949c3dc87ab392ca3dc454
Franz Becker <franz.becker@itemis.de>
2015-11-27 14:55:55 +0100

    demo 1

---
+++ playground/src/test/java/HelloWorldTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+/*
+    @Test
+    public void testGreeting() {
+        // given
+        HelloWorld helloWorld = new HelloWorld();
+
+        // when
+        String greeting = helloWorld.getGreeting();
+
+        // then
+        assertEquals("Hello world", greeting);
+    }
+*/
+}
